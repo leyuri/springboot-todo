8b57ff1eddc83bca4d35ac58e71cf847e3548043
leyuri <leyuri97@gmail.com>
2021-06-03 12:27:53 +0900

    Add docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ LABEL maintainer="leyuri97@gmail.com"
 VOLUME /tmp
 
 # Make port 8080 available to the world outside this container
-EXPOSE 8080
+EXPOSE 5000
 
 # The application's jar file
 ARG JAR_FILE=build/libs/demo-0.0.1-SNAPSHOT.jar
@@ -19,4 +19,3 @@ ADD ${JAR_FILE} to-do-springboot.jar
 
 # Run the jar file
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/to-do-springboot.jar"]
-
--- src/main/java/com/spring/todo/TodoApplication.java
+++ src/main/java/com/spring/todo/TodoApplication.java
@@ -1,16 +1,11 @@
 package com.spring.todo;
 
-import com.spring.todo.item.ToDoItemRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = ToDoItemRepository.class)
 public class TodoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
     }
-
 }
